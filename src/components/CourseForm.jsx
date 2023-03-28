7b5cbfeb7009ba3466bd1af9b3a8fbfb72736504
yashPundhir <yash.pundhir.prof@gmail.com>
2023-03-28 20:02:28 +0530

    CourseForm Component Added & Store logic applied

---
+++ src/components/CourseForm.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import useCourseStore from "../app/courseStore";
+
+import "../App.css";
+
+const CourseForm = () => {
+	const addCourse = useCourseStore((state) => state.addCourse);
+
+	const [courseTitle, setCourseTitle] = useState("");
+
+	const handleCourseSubmit = () => {
+		if (!courseTitle) {
+			return alert("please add course title");
+		}
+		addCourse({
+			id: Math.ceil(Math.random() * 1000000),
+			title: courseTitle,
+		});
+		setCourseTitle("");
+	};
+
+	console.log("Course Form Rendered");
+
+	return (
+		<div className="form-container">
+			<input
+				type="text"
+				className="form-input"
+				value={courseTitle}
+				onChange={(e) => {
+					setCourseTitle(e.target.value);
+				}}
+			/>
+			<button
+				className="form-submit-btn"
+				onClick={() => {
+					handleCourseSubmit();
+				}}
+			>
+				Add Course
+			</button>
+		</div>
+	);
+};
+
+export default CourseForm;
